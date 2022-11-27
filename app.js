a12c7bb2d29058ae4b8f8386acd4efb1186a90d4
Juan Luis Ayala Jr <113149815+JuanAyala7@users.noreply.github.com>
2022-11-27 14:02:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -15,12 +16,40 @@ app.get("/", function(req, res){
 
 app.post("/", function(req, res){
 
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  };
 
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us8.api.mailchimp.com/3.0/lists/3814e02ac8";
+
+  const options = {
+    method: "POST",
+    auth: "ayala98:6bbe06690988bccbacbf4d8a11610d3b-us8"
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
 
 });
 
@@ -28,3 +57,9 @@ app.post("/", function(req, res){
 app.listen(3000, function() {
   console.log("Server is running on port 3000.");
 });
+
+//API key
+// 6bbe06690988bccbacbf4d8a11610d3b-us8
+
+//List Id
+// 3814e02ac8
